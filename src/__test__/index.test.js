989bcb99eb6a5db9f2abd8e3925cad758e5559b7
Ash <redcoolike@gmail.com>
2024-11-11 15:36:51 +0100

    damageTest

--- src/__test__/index.test.js
+++ src/__test__/index.test.js
@@ -3,3 +3,10 @@ import Character from '../character';
 test('создание несуществующего персонажа', () => {
   expect(() => new Character('Ivan', 'Baba Yaga')).toThrow('недопустимый тип персонажа');
 });
+
+test('test damage', () => {
+  const newChar = new Character('Bonch','Bowman');
+  newChar.health = 0;
+  newChar.damage(2)
+  expect(newChar.health).toBe(0)
+})
--- src/character.js
+++ src/character.js
@@ -29,7 +29,7 @@ export default class Character {
   }
 
   damage(points) {
-    if (this.health >= 0) {
+    if (this.health > 0) {
       this.health -= points * (1 - this.defence / 100);
     }
   }
